603ce4e1450c90c36b4b5ba6cca9b71fa88ceb80
HyrupDesign <Mikkelhyrup@hotmail.com>
2025-05-21 15:22:38 +0200

    lidt størrelser ændret, samt et forsøg på at lægge billede slide ind

--- dropdown.js
+++ dropdown.js
@@ -22,3 +22,40 @@ window.onclick = function (event) {
   }
 };
 */
+
+/*                    */
+/*   billede slide    */
+/*                    */
+
+const slideIndices = {};
+
+function openSlideshow(id) {
+  document.getElementById(id).style.display = "block";
+  if (!(id in slideIndices)) {
+    slideIndices[id] = 0;
+  }
+  showSlide(slideIndices[id], id);
+}
+
+function closeSlideshow(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function changeSlide(n, id) {
+  slideIndices[id] += n;
+  showSlide(slideIndices[id], id);
+}
+
+function showSlide(n, id) {
+  const container = document.getElementById(id);
+  const slides = container.querySelectorAll(".slide");
+
+  if (n >= slides.length) slideIndices[id] = 0;
+  if (n < 0) slideIndices[id] = slides.length - 1;
+
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+
+  slides[slideIndices[id]].style.display = "block";
+}
